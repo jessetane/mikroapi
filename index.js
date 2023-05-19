136a6c007eb12281fa25ffd41ee1c6c2cde63138
Jesse Tane <jesse.tane@gmail.com>
2023-05-19 17:05:11 -0400

    modernize

--- index.js
+++ index.js
@@ -1,216 +1,234 @@
-var net = require('net')
-var tls = require('tls')
-var crypto = require('crypto')
+import net from 'net'
+import tls from 'tls'
+import crypto from 'crypto'
 
-module.exports = class MikroApi {
-  constructor (opts = {}) {
-    opts.timeout = opts.timeout || 5000
-    this.opts = opts
-  }
-
-  connect (cb) {
-    if (this.connection) {
-      return cb()
-    } else {
-      delete this.closed
-    }
-    this.queue = [cb]
-    this.connection = this.opts.tls
-      ? tls.connect(this.opts.port, this.opts.host, this.opts.tls)
-      : net.connect(this.opts.port, this.opts.host)
-    this.connection.on('error', err => this.onclose(err))
-    this.connection.on('close', () => this.onclose())
-    this.connection.on('connect', () => this.login())
-    var buffer = ''
-    var state = null
-    var response = null
-    var attributes = null
-    this.connection.on('data', data => {
-      clearTimeout(this.timer)
-      buffer += data
-      while (buffer.length) {
-        var length = this.decodeLength(buffer)
-        if (length < 0) return
-        var offset = length[0]
-        buffer = buffer.slice(offset)
-        length = length[1]
-        if (length === 0) {
-          if (state === '!re') {
-            response.push(attributes)
-            attributes = null
-          } else {
-            var res = Array.isArray(response) ? response : attributes
-            response = attributes = null
-            var cb = this.queue.shift()
-            cb(null, res)
-          }
-        }
-        if (buffer.length === 0) return
-        var word = buffer.slice(0, length)
-        buffer = buffer.slice(length)
-        if (word.indexOf('!') === 0) {
-          if (word === '!fatal') {
-            this.close()
-            length = this.decodeLength(buffer)
-            offset = length[0]
-            length = length[1]
-            var message = buffer.slice(offset, offset + length)
-            var cb = this.queue.shift()
-            cb(new Error(word + ' ' + message))
-            return  
-          } else {
-            state = word
-          }
-        } else {
-          if (!response) {
-            if (state === '!re') {
-              response = []
-            }
-          }
-          if (!attributes) {
-            attributes = {}
-          }
-          if (word) {
-            var parts = word.split('=')
-            attributes[parts[1]] = parts[2]
-          }
-        }
-      }
-    })
-  }
-
-  exec (command, params, cb) {
-    if (typeof params === 'function') {
-      cb = params
-      params = {}
-    }
-    this.queue.push(cb)
-    var sentence = ''
-    sentence += this.encodeLength(command) + command
-    for (var key in params) {
-      var word = key + '=' + params[key]
-      if (key[0] !== '?') word = '=' + word
-      sentence += this.encodeLength(word) + word
-    }
-    sentence += '\x00'
-    this.connection.write(sentence)
-    clearTimeout(this.timer)
-    this.timer = setTimeout(() => {
-      this.connection.destroy()
-    }, this.opts.timeout)
-  }
-
-  login () {
-    var cb = this.queue.shift()
-    this.exec('/login', (err, res) => {
-      if (err) return cb(err)
-      var hash = crypto.createHash('MD5')
-      hash.update('\x00')
-      hash.update(this.opts.password)
-      hash.update(new Buffer(res.ret, 'hex'))
-      this.exec('/login', {
-        name: this.opts.username,
-        response: '00' + hash.digest('hex')
-      }, cb)
-    })
-  }
-
-  onclose (err) {
-    if (this.closed || !this.connection) return
-    if (!err) err = new Error('connection timeout')
-    this.teardown(err)
-  }
-
-  close () {
-    this.closed = true
-    this.teardown(new Error('connection closed'))
-  }
-
-  teardown (err) {
-    clearTimeout(this.timer)
-    this.connection.destroy()
-    delete this.connection
-    while (this.queue.length) this.queue.shift()(err)
-  }
-
-  encodeLength (word) {
-    var length = word.length
-    var buf = []
-    if (length < 0x80) {
-      buf.push(length)
-    } else if (length < 0x4000) {
-      length |= 0x8000
-      buf.push(
-        (length >> 8) & 0xFF,
-        length & 0xFF
-      )
-    } else if (length < 0x200000) {
-      length |= 0xC00000
-      buf.push(
-        (length >> 16) & 0xFF,
-        (length >> 8) & 0xFF,
-        length & 0xFF
-      )
-    } else if (length < 0x10000000) {
-      length |= 0xE0000000
-      buf.push(
-        (length >> 24) & 0xFF,
-        (length >> 16) & 0xFF,
-        (length >> 8) & 0xFF,
-        length & 0xFF
-      )
-    } else {
-      buf.push(
-        0xF0,
-        (length >> 24) & 0xFF,
-        (length >> 16) & 0xFF,
-        (length >> 8) & 0xFF,
-        length & 0xFF
-      )
-    }
-    return buf.map(b => String.fromCodePoint(b)).join('')
-  }
-
-  decodeLength (word) {
-    word = new Buffer(word)
-    var length = word[0]
-    if ((length & 0x80) == 0x00) {
-      length = word[0]
-      return [1,length]
-    } else if ((length & 0xC0) == 0x80) {
-      if (word.length < 2) return -1
-      length &= ~0xC0
-      length <<= 8
-      length += word[1]
-      return [2,length]
-    } else if ((length & 0xE0) == 0xC0) {
-      if (word.length < 3) return -1
-      length &= ~0xE0
-      length <<= 8
-      length += word[1]
-      length <<= 8
-      length += word[2]
-      return [3,length]
-    } else if ((length & 0xF0) == 0xE0) {
-      if (word.length < 4) return -1
-      length &= ~0xF0
-      length <<= 8
-      length += word[1]
-      length <<= 8
-      length += word[2]
-      length <<= 8
-      length += word[3]
-      return [4,length]
-    } else if ((length & 0xF8) == 0xF0) {
-      if (word.length < 5) return -1
-      length = word[1]
-      length <<= 8
-      length += word[2]
-      length <<= 8
-      length += word[3]
-      length <<= 8
-      length += word[4]
-      return [5,length]
-    }
-  }
+function Deferred () {
+	let s, f, p = new Promise((_s, _f) => { s = _s; f = _f })
+	p.resolve = s
+	p.reject = f
+	return p
 }
+
+class MikroApi {
+	constructor (opts = {}) {
+		opts.timeout = opts.timeout || 5000
+		this.opts = opts
+		this.buffer = ''
+		this.state = null
+		this.response = null
+		this.attributes = null
+		this.onclose = this.onclose.bind(this)
+		this.ondata = this.ondata.bind(this)
+	}
+
+	connect () {
+		if (this.connection) return
+		delete this.closed
+		const p = new Deferred()
+		this.queue = []
+		this.connection = this.opts.tls
+			? tls.connect(this.opts.port, this.opts.host, this.opts.tls)
+			: net.connect(this.opts.port, this.opts.host)
+		this.connection.on('error', this.onclose)
+		this.connection.on('close', this.onclose)
+		this.connection.on('data', this.ondata)
+		this.connection.on('connect', async () => {
+			try {
+				const res = await this.login()
+				p.resolve()
+			} catch (err) {
+				p.reject(err)
+			}
+		})
+		return p
+	}
+
+	ondata (data) {
+		clearTimeout(this.timer)
+		this.buffer += data
+		while (this.buffer.length) {
+			let length = this.decodeLength(this.buffer)
+			if (length < 0) return
+			let offset = length[0]
+			this.buffer = this.buffer.slice(offset)
+			length = length[1]
+			if (length === 0) {
+				if (this.state === '!re') {
+					this.response.push(this.attributes)
+					this.attributes = null
+				} else {
+					const res = Array.isArray(this.response) ? this.response : this.attributes
+					this.response = this.attributes = null
+					const p = this.queue.shift()
+					if (p) {
+						if (res?.message) {
+							p.reject(new Error(res.message))
+						} else {
+							p.resolve(res)
+						}
+					}
+				}
+			}
+			if (this.buffer.length === 0) return
+			const word = this.buffer.slice(0, length)
+			this.buffer = this.buffer.slice(length)
+			if (word.indexOf('!') === 0) {
+				if (word === '!fatal') {
+					length = this.decodeLength(buffer)
+					offset = length[0]
+					length = length[1]
+					var message = this.buffer.slice(offset, offset + length)
+					const p = this.queue.shift()
+					if (p) p.reject(new Error(word + ' ' + message))
+					this.close()
+					return
+				} else {
+					this.state = word
+				}
+			} else {
+				if (!this.response) {
+					if (this.state === '!re') {
+						this.response = []
+					}
+				}
+				if (!this.attributes) {
+					this.attributes = {}
+				}
+				if (word) {
+					const parts = word.split('=')
+					this.attributes[parts[1]] = parts[2]
+				}
+			}
+		}
+	}
+
+	exec (command, params) {
+		const p = new Deferred()
+		this.queue.push(p)
+		this.connection.write(this.encodeLength(command))
+		this.connection.write(command)
+		for (var key in params) {
+			var word = key + '=' + params[key]
+			if (key[0] !== '?') word = '=' + word
+			this.connection.write(this.encodeLength(word))
+			this.connection.write(word)
+		}
+		this.connection.write('\x00')
+		clearTimeout(this.timer)
+		this.timer = setTimeout(() => {
+			this.connection.destroy()
+		}, this.opts.timeout)
+		return p
+	}
+
+	async login () {
+		return this.exec('/login', {
+			name: this.opts.username,
+			password: this.opts.password
+		})
+	}
+
+	onclose (err) {
+		if (this.closed || !this.connection) return
+		if (!err) err = new Error('connection timeout')
+		this.teardown(err)
+	}
+
+	close () {
+		this.closed = true
+		this.teardown(new Error('connection closed'))
+	}
+
+	teardown (err) {
+		clearTimeout(this.timer)
+		this.connection.destroy()
+		delete this.connection
+		while (this.queue.length) {
+			const p = this.queue.shift()
+			p.reject(err)
+		}
+	}
+
+	encodeLength (word) {
+		var length = word.length
+		var buf = []
+		if (length < 0x80) {
+			buf.push(length)
+		} else if (length < 0x4000) {
+			length |= 0x8000
+			buf.push(
+				(length >> 8) & 0xFF,
+				length & 0xFF
+			)
+		} else if (length < 0x200000) {
+			length |= 0xC00000
+			buf.push(
+				(length >> 16) & 0xFF,
+				(length >> 8) & 0xFF,
+				length & 0xFF
+			)
+		} else if (length < 0x10000000) {
+			length |= 0xE0000000
+			buf.push(
+				(length >> 24) & 0xFF,
+				(length >> 16) & 0xFF,
+				(length >> 8) & 0xFF,
+				length & 0xFF
+			)
+		} else {
+			buf.push(
+				0xF0,
+				(length >> 24) & 0xFF,
+				(length >> 16) & 0xFF,
+				(length >> 8) & 0xFF,
+				length & 0xFF
+			)
+		}
+		return new Uint8Array(buf)
+	}
+
+	decodeLength (word) {
+		word = Buffer.from(word)
+		var length = word[0]
+		if ((length & 0x80) == 0x00) {
+			length = word[0]
+			return [1,length]
+		} else if ((length & 0xC0) == 0x80) {
+			if (word.length < 2) return -1
+			length &= ~0xC0
+			length <<= 8
+			length += word[1]
+			return [2,length]
+		} else if ((length & 0xE0) == 0xC0) {
+			if (word.length < 3) return -1
+			length &= ~0xE0
+			length <<= 8
+			length += word[1]
+			length <<= 8
+			length += word[2]
+			return [3,length]
+		} else if ((length & 0xF0) == 0xE0) {
+			if (word.length < 4) return -1
+			length &= ~0xF0
+			length <<= 8
+			length += word[1]
+			length <<= 8
+			length += word[2]
+			length <<= 8
+			length += word[3]
+			return [4,length]
+		} else if ((length & 0xF8) == 0xF0) {
+			if (word.length < 5) return -1
+			length = word[1]
+			length <<= 8
+			length += word[2]
+			length <<= 8
+			length += word[3]
+			length <<= 8
+			length += word[4]
+			return [5,length]
+		}
+	}
+}
+
+export default MikroApi
--- readme.md
+++ readme.md
@@ -9,25 +9,20 @@ Just a very basic port of the python implementation shown on the wiki.
 
 ## Example
 ``` javascript
-var api = new MikroApi({
-  host: '1.1.1.1',
-  port: 8729,
-  username: 'admin',
-  password: 'xxx',
-  tls: {
-    rejectUnauthorized: false // maybe don't do this
-  }
+const api = new MikroApi({
+	host: '1.1.1.1',
+	port: 8729,
+	username: 'admin',
+	password: 'xxx',
+	tls: {
+		rejectUnauthorized: false // maybe don't do this
+	}
 })
-
-api.connect(err => {
-  if (err) throw err
-  api.exec('/routing/bgp/peer/print', {
-    '.proplist': 'prefix-count,disabled,state,uptime,remote-as,remote-address'
-  }, (err, res) => {
-    if (err) throw err
-    console.log(res)
-  })
+await api.connect()
+const res = await api.exec('/routing/bgp/peer/print', {
+	'.proplist': 'prefix-count,disabled,state,uptime,remote-as,remote-address'
 })
+console.log(res)
 ```
 
 ## License
